1da98164bd328d22ad86e052c97f10075b472b1b
xdd <45672942@qq.com>
2022-08-28 15:01:08 +0800

    webpack打包

---
+++ code/part3/webpack.config.js
@@ -0,0 +1,29 @@
+//引入一个包
+const path=require('path');
+//webpack所有的配置信息都用改写在module.exports中
+module.exports={
+    //指定入口文件
+    entry:'./src/index.ts',
+
+    //指定文件所在目录
+    output: {
+        //指定打包后的路径
+        path: path.resolve(__dirname,'dist'),
+        //打包后文件的名称
+        filename: "bundle.js"
+    },
+    //指定webpack打包时要使用的模块
+    module: {
+        //指定要加载的规则
+        rules: [{
+            //test指定规则生效的文件
+            test:/\.ts$/,
+            //要使用的loader怎么去处理ts文件
+            use: 'ts-loader',
+            //要排除的文件
+            exclude: /node-modules/
+
+        }]
+    }
+
+};
